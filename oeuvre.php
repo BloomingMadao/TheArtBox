d41cb3f2479958dc962b36bb66b5525d34cd70a8
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-05-20 15:23:18 +0200

    Fix oeuvre.php

--- oeuvre.php
+++ oeuvre.php
@@ -3,14 +3,24 @@
 $getData = $_GET;
 $id=$getData['id'];
 
+$o = null;
 
-if(
-    !isset($getData) ||
-    !in_array($id,$oeuvres[$id-1])
-    
-){
-    echo 'L\'oeuvre n\'existe pas';
+foreach($oeuvres as $oeuvre){
+    if ($id == $oeuvre['id']){
+        $o = $oeuvre;
+    }
 }
+//DEBUG
+// echo '<pre>';
+//     print_r($o);
+// echo '</pre>';
+// if(
+//     !isset($getData) ||
+//     !in_array($id,$oeuvres[$id-1])
+    
+// ){
+//     echo 'L\'oeuvre n\'existe pas';
+// }
 
 
 ?>
@@ -19,13 +29,13 @@ if(
 <?php include('header.php');?>
     <article id="detail-oeuvre">
         <div id="img-oeuvre">
-            <img src="<?php echo $oeuvres[$id-1]['img']; ?>" alt="<?php echo $oeuvres[$id-1]['title']; ?>">
+            <img src="<?php echo $o['img']; ?>" alt="<?php echo $o['title']; ?>">
         </div>
         <div id="contenu-oeuvre">
-            <h1><?php echo $oeuvres[$id-1]['title']; ?></h1>
-            <p class="description"><?php echo $oeuvres[$id-1]['author']; ?></p>
+            <h1><?php echo $o['title']; ?></h1>
+            <p class="description"><?php echo $o['author']; ?></p>
             <p class="description-complete">
-                <?php echo $oeuvres[$id-1]['description']; ?>
+                <?php echo $o['description']; ?>
             </p>
         </div>
     </article>
