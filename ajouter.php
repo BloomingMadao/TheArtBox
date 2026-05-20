c1b68ec13c0c37b0e9f0ad035c2924893e760018
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-05-20 15:42:04 +0200

    Fix parameters

--- ajouter.php
+++ ajouter.php
@@ -2,16 +2,16 @@
 
 <form action="traitement.php" method="POST">
     <div class="champ-formulaire">
-        <label for="titre">Titre de l'œuvre</label>
-        <input type="text" name="titre" id="titre">
+        <label for="title">Titre de l'œuvre</label>
+        <input type="text" name="title" id="title">
     </div>
     <div class="champ-formulaire">
-        <label for="artiste">Auteur de l'œuvre</label>
-        <input type="text" name="artiste" id="artiste">
+        <label for="author">Auteur de l'œuvre</label>
+        <input type="text" name="author" id="author">
     </div>
     <div class="champ-formulaire">
-        <label for="image">URL de l'image</label>
-        <input type="url" name="image" id="image">
+        <label for="img">URL de l'image</label>
+        <input type="url" name="img" id="img">
     </div>
     <div class="champ-formulaire">
         <label for="description">Description</label>
--- oeuvre.php
+++ oeuvre.php
@@ -26,11 +26,11 @@
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= $oeuvre['img'] ?>" alt="<?= $oeuvre['title'] ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= $oeuvre['title'] ?></h1>
+        <p class="description"><?= $oeuvre['author'] ?></p>
         <p class="description-complete">
              <?= $oeuvre['description'] ?>
         </p>
