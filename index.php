7f807df533629999a47193bfc407be594e0abe9a
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-05-20 15:13:52 +0200

    Add id in url in index.php and add 'oeuvre.php' page that display work art details in one page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div id="liste-oeuvres">
     <?php foreach ($oeuvres as $oeuvre) : ?>
         <article class="oeuvre">
-            <a href="oeuvre-<?php echo $oeuvre['id']; ?>.php">
+            <a href="oeuvre.php?id=<?php echo $oeuvre['id']; ?>">
                 <img src="<?php echo $oeuvre['img']; ?>" alt="<?php echo $oeuvre['title']; ?>">
                 <h2><?php echo $oeuvre['title']; ?></h2>
                 <p class="description"><?php echo $oeuvre['author']; ?></p>
--- oeuvre.php
+++ oeuvre.php
@@ -1,2 +1,32 @@
 <?php
+ include('oeuvres.php');
+$getData = $_GET;
+$id=$getData['id'];
 
+
+if(
+    !isset($getData) ||
+    !in_array($id,$oeuvres[$id-1])
+    
+){
+    echo 'L\'oeuvre n\'existe pas';
+}
+
+
+?>
+
+
+<?php include('header.php');?>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="<?php echo $oeuvres[$id-1]['img']; ?>" alt="<?php echo $oeuvres[$id-1]['title']; ?>">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1><?php echo $oeuvres[$id-1]['title']; ?></h1>
+            <p class="description"><?php echo $oeuvres[$id-1]['author']; ?></p>
+            <p class="description-complete">
+                <?php echo $oeuvres[$id-1]['description']; ?>
+            </p>
+        </div>
+    </article>
+<?php include('footer.php');?>
